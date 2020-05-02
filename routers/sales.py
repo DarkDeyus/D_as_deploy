38ca296c0d5ffd7ad79d177e5367064e8d832c2a
DarkDev <21362600+DarkDeyus@users.noreply.github.com>
2020-05-02 23:30:57 +0200

    Add and /sales endpoint for genres

--- routers/sales.py
+++ routers/sales.py
@@ -12,14 +12,22 @@ async def create_album(category: str, response: Response,
 
     if category == "customers":
         connection.row_factory = aiosqlite.Row
-        cursor = await connection.execute("SELECT invoices.CustomerId, customers.Email, customers.Phone,"
-                                          " ROUND(SUM(invoices.Total), 2) as Sum FROM invoices"
-                                          " JOIN customers ON invoices.CustomerId = customers.CustomerId"
-                                          " GROUP BY invoices.CustomerId"
-                                          " ORDER BY Sum DESC, invoices.CustomerId")
+        cursor = await connection.execute("SELECT invoices.CustomerId, customers.Email, customers.Phone, "
+                                          "ROUND(SUM(invoices.Total), 2) as Sum FROM invoices "
+                                          "JOIN customers ON invoices.CustomerId = customers.CustomerId "
+                                          "GROUP BY invoices.CustomerId "
+                                          "ORDER BY Sum DESC, invoices.CustomerId")
         stats = await cursor.fetchall()
         return stats
-
+    elif category == "genres":
+        connection.row_factory = aiosqlite.Row
+        cursor = await connection.execute("SELECT genres.Name as Name, Count(genres.GenreId) as Sum FROM invoice_items "
+                                          "JOIN tracks ON invoice_items.TrackId = tracks.TrackId "
+                                          "JOIN genres ON tracks.GenreId = genres.GenreId "
+                                          "GROUP BY genres.GenreId "
+                                          "ORDER BY Sum DESC, Name")
+        stats = await cursor.fetchall()
+        return stats
     else:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"detail": {"error": "Given category does not exist"}}
