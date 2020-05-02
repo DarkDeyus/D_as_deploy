d46da411024fc942bcd23bcae32c40fe9c260df2
DarkDev <21362600+DarkDeyus@users.noreply.github.com>
2020-05-02 21:54:43 +0200

    Changed path to database

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import aiosqlite
 
-DB_ADDRESS: str = r"D:\Daftcode - kurs python\D_as_deploy\chinook.db"
+# DB_ADDRESS: str = r"D:\Daftcode - kurs python\D_as_deploy\chinook.db"
+DB_ADDRESS: str = r"D_as_deploy\chinook.db"
 DB_CONNECTION: aiosqlite.Connection = None
 
 
